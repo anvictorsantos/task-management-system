530549b4cd9e413cd69e51e3379127339ce24986
Antonio Pontes <developer.antonio.pontes@gmail.com>
2024-03-09 17:23:19 +0000

    feat: all unit tests coverage

--- src/test/java/com/devtonho/task/service/TaskServiceTest.java
+++ src/test/java/com/devtonho/task/service/TaskServiceTest.java
@@ -3,7 +3,6 @@ package com.devtonho.task.service;
 import com.devtonho.task.entity.Task;
 import com.devtonho.task.repository.TaskRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -11,9 +10,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceTest {
@@ -54,8 +57,37 @@ class TaskServiceTest {
     }
 
     @Test
-    @Disabled
-    void saveTasks() {
+    void canSaveTasks() {
+        // given
+        List<Task> tasks = Arrays.asList(
+                new Task(
+                        1,
+                        "Title 1",
+                        "Description for Task 1",
+                        LocalDateTime.now(),
+                        "In Progress"
+                ),
+                new Task(
+                        2,
+                        "Title 2",
+                        "Description for Task 2",
+                        LocalDateTime.now(),
+                        "In Progress"
+                )
+        );
+
+        // when
+        underTest.saveTasks(tasks);
+
+        // then
+        ArgumentCaptor<List<Task>> listTaskArgumentCaptor =
+                ArgumentCaptor.forClass(List.class);
+
+        verify(taskRepository).saveAll(listTaskArgumentCaptor.capture());
+
+        List<Task> captorValues = listTaskArgumentCaptor.getValue();
+
+        assertThat(captorValues).isEqualTo(tasks);
     }
 
     @Test
@@ -92,12 +124,41 @@ class TaskServiceTest {
     }
 
     @Test
-    @Disabled
-    void deleteTask() {
+    void canDeleteTask() {
+        // given
+        int id = 1;
+
+        // when
+        underTest.deleteTask(id);
+
+        // then
+        verify(taskRepository).deleteById(id);
     }
 
     @Test
-    @Disabled
-    void updateTask() {
+    void canUpdateTask() {
+        // given
+        Task task = new Task(
+                1,
+                "Title 1",
+                "Description for Task 1",
+                LocalDateTime.now(),
+                "In Progress"
+        );
+
+        // when
+        when(taskRepository.findById(task.getId())).thenReturn(Optional.of(task));
+        underTest.updateTask(task);
+
+        // then
+        ArgumentCaptor<Task> taskArgumentCaptor =
+                ArgumentCaptor.forClass(Task.class);
+
+        verify(taskRepository)
+                .save(taskArgumentCaptor.capture());
+
+        Task captorValue = taskArgumentCaptor.getValue();
+
+        assertThat(captorValue).isEqualTo(task);
     }
 }
