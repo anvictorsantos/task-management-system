abab15e21f0a0a8cb3b11f9e698c560a377002d7
Antonio Pontes <developer.antonio.pontes@gmail.com>
2024-03-09 10:11:34 +0000

    feat: canSaveTask unit test

--- src/test/java/com/devtonho/task/service/TaskServiceTest.java
+++ src/test/java/com/devtonho/task/service/TaskServiceTest.java
@@ -1,15 +1,20 @@
 package com.devtonho.task.service;
 
+import com.devtonho.task.entity.Task;
 import com.devtonho.task.repository.TaskRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,8 +30,29 @@ class TaskServiceTest {
     }
 
     @Test
-    @Disabled
-    void saveTask() {
+    void canSaveTask() {
+        // given
+        Task task = new Task(
+                1,
+                "Title 1",
+                "Description for Task 1",
+                LocalDateTime.now(),
+                "In Progress"
+        );
+
+        // when
+        underTest.saveTask(task);
+
+        // then
+        ArgumentCaptor<Task> taskArgumentCaptor =
+                ArgumentCaptor.forClass(Task.class);
+
+        verify(taskRepository)
+                .save(taskArgumentCaptor.capture());
+
+        Task captorValue = taskArgumentCaptor.getValue();
+
+        assertThat(captorValue).isEqualTo(task);
     }
 
     @Test
